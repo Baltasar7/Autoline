518f43b40960f03354b9ddbde4f62181d6ea0190
Ohno <mandheling200@gmail.com>
2019-09-16 11:59:53 +0900

    bootstrap

--- routes/lineCommercial.js
+++ routes/lineCommercial.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('hello', {
+  res.render('lineCommercial', {
     msg: 'Hello Autoline!!'
   });
 });
